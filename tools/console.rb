f6d7fc323002558a229de781c95052bde7d39dc8
Babechick13 <jessicanatovich@yahoo.com>
2020-01-13 10:18:51 -0500

    done

--- tools/console.rb
+++ tools/console.rb
@@ -10,6 +10,17 @@ c1=Company.new("Arizona",1923)
 c2=Company.new("Seatle",1999)
 c3=Company.new("Washington",1922)
 
+d1=Dev.new("Sally")
+d2=Dev.new("Marry")
+d3=Dev.new("Andrew")
+
+# item_name, :company, :dev
+f1=Freebie.new("pen",c1,d1)
+f2=Freebie.new("pencil",c2,d2)
+f3=Freebie.new("paper",c3,d3)
+
+
+
 
 
 
