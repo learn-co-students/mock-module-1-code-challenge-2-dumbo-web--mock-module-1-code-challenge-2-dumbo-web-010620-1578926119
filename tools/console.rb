ef1de22023318fda72dc03d94c48b6d8d89a60b1
github username <github email address>
2020-01-13 10:49:21 -0500

    scared for Coding Challenge, I feel NOT ready

--- tools/console.rb
+++ tools/console.rb
@@ -10,8 +10,19 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+# ------------------Devs----------------------------
+elon = Dev.new("Elon Musk")
+bill = Dev.new("Bill Gates")
+steve = Dev.new("Steve Jobs")
 
+# ------------------Company-------------------------
+tesla = Company.new("Tesla", 2003)
+microsoft = Company.new("Microsoft", 1975)
+apple = Company.new("Apple", 1976)
 
+# ------------------freebie-------------------------
+cc_cookie = Freebie.new("Chocolate Chip Cookie", microsoft, bill)
+rb_cookie = Freebie.new("Raisin Bran Cookie", tesla, elon)
 
 
 # ===== WARNING! DO NOT EDIT BELOW THIS LINE ===== #
