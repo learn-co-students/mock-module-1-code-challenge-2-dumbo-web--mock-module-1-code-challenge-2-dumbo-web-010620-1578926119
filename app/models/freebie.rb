4ce4278415236fe0c4e23b71be756804e3390d29
github username <github email address>
2020-01-13 10:49:30 -0500

    submit mockchallenge

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,3 +1,35 @@
 class Freebie
+    attr_accessor :item_name, :company, :dev
+
+    @@all = []
+
+    def initialize(item_name, company, dev)
+        @item_name = item_name
+        @company = company
+        @dev = dev
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    
 
 end
+
+# ************ TO DO **************
+
+
+
+  # ************ DONE **************
+#  `Freebie#initialize(item_name, company, dev)`
+#   - should initialize with an item name (string), company (`Company` instance) and dev (`Dev` instance)
+#  `Freebie#item_name`
+#   - should return the name of the item, ie. `"sticker"` or `"water bottle"`
+#  `Freebie.all`
+#   - Returns an array of all Freebie instances
+# `Freebie#dev`
+#   - should return the `Dev` instance for this freebie
+# `Freebie#company`
+#   - should return the `Company` instance for this freebie
