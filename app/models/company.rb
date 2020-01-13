e5f3040c6db78926439f74dd36c71fd0ec6118b0
Babechick13 <jessicanatovich@yahoo.com>
2020-01-13 10:10:31 -0500

    done

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,9 +1,24 @@
 class Company
   attr_reader :name, :founding_year
-
+  @@all = []
   def initialize(name, founding_year)
     @name = name
     @founding_year = founding_year
+    @@all << self
   end
-
-end
+  def self.all
+    end 
+  def freebies
+    freebies.map do |freebies|
+    freebies.company
+      end
+   end
+   def companies
+    self.dev.map(&:companies).uniq
+  end
+  def company
+    Freebie.all.select do |freebie_instance|
+    freebie_instance.company == self
+    end
+  end
+  end
--- tools/console.rb
+++ tools/console.rb
@@ -5,10 +5,11 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
+# name, :founding_year
+c1=Company.new("Arizona",1923)
+c2=Company.new("Seatle",1999)
+c3=Company.new("Washington",1922)
 
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
 
 
 
