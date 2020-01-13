6d9984ad1337439736481e128b58d55e2edbd2d2
wenglin1 <wenglin@buffalo.edu>
2020-01-13 10:47:53 -0500

    done

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,3 +1,20 @@
 class Freebie
+    @attr_reader :item_name, :company, :dev
 
+    @@all = []
+    def initialize(item_name,company,dev)
+        @item_name = item_name 
+        @company = Company.new
+        @dev = Dev.new
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def print_details
+        "#{@dev} owns a #{@item_name} from #{@company}"
+    end
 end
