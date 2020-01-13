15589e7d9afb145688041413f584a24862c06ff1
github username <github email address>
2020-01-13 10:36:53 -0500

    Done with Company#give_freebie(dev, item_name)

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,9 +1,29 @@
 class Company
   attr_reader :name, :founding_year
 
+  @@all = [] 
+
   def initialize(name, founding_year)
     @name = name
     @founding_year = founding_year
+
+    @@all << self 
   end
+
+  def self.all 
+    @@all
+  end
+
+  # def freebies
+    
+  # end
+
+  def give_freebie(dev, item_name)
+    Freebie.new(item_name, self, dev)
+  end 
+
+  # Freebie.new(item_name, company, dev)
+
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -9,8 +9,24 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+# ruby tools/console.rb 
 
 
+# Company.new(name, founding_year)
+microsoft = Company.new("Microsoft", 1850)
+apple = Company.new("Apple", 1950)
+bloomberg = Company.new("Bloomberg", 2000)
+
+
+# Dev.new(name)
+theo = Dev.new("Theo")
+mike = Dev.new("Mike")
+steven = Dev.new("Steven")
+
+
+
+
+f1 = bloomberg.give_freebie(mike, "sticker")
 
 
 
