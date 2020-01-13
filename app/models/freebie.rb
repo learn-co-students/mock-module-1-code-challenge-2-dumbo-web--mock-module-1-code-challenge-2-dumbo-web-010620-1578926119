b7d6420b7bf44c089266e45bc14ca427fe3b4e1e
lsamano <leizl.samano@gmail.com>
2020-01-13 15:28:48 -0500

    fixed item_name name

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -20,6 +20,6 @@ class Freebie
   end
 
   def print_details
-    "#{self.dev.name} owns a #{self.item_name} from #{self.company.name}"
+    "#{self.dev.name} owns a #{self.name} from #{self.company.name}"
   end
 end
